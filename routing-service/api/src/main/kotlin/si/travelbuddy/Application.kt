66d2e08bd6676de2c24b59e35b74cc6cc68457bb
nikolabr <nikolabrk@protonmail.com>
2024-12-28 19:40:02 +0100

    Updated routing-service config

--- routing-service/api/src/main/kotlin/si/travelbuddy/Application.kt
+++ routing-service/api/src/main/kotlin/si/travelbuddy/Application.kt
@@ -1,9 +1,12 @@
 package si.travelbuddy
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 }
 
 fun Application.module() {
