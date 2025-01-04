d2eac10bc595b64f7a327e8ee57509f2ed3663dd
nikolabr <nikolabrk@protonmail.com>
2025-01-04 16:46:41 +0100

    Added DepartureDelayDao

--- crowdsourcing-service/entity/build.gradle.kts
+++ crowdsourcing-service/entity/build.gradle.kts
@@ -1,3 +1,8 @@
+val exposed_version: String by project
+val h2_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.kotlin.plugin.serialization")
@@ -11,6 +16,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0-RC")
     testImplementation(kotlin("test"))
 }
 
--- crowdsourcing-service/service/build.gradle.kts
+++ crowdsourcing-service/service/build.gradle.kts
@@ -1,3 +1,8 @@
+val exposed_version: String by project
+val h2_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.kotlin.plugin.serialization")
