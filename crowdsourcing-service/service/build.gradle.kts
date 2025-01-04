9af1ee62499b6471cb529cbcfad1d3a4dd8c7eb5
nikolabr <nikolabrk@protonmail.com>
2025-01-04 16:32:23 +0100

    Joined pluginManagement

--- crowdsourcing-service/service/build.gradle.kts
+++ crowdsourcing-service/service/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 group = "si.travelbuddy.crowdsourcing"
--- crowdsourcing-service/settings.gradle.kts
+++ crowdsourcing-service/settings.gradle.kts
@@ -1,7 +1,17 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+
+pluginManagement {
+    plugins {
+        val kotlin_version: String by settings
+        val ktor_version: String by settings
+
+        kotlin("jvm") version kotlin_version
+        id("io.ktor.plugin") version ktor_version
+        id("org.jetbrains.kotlin.plugin.serialization") version kotlin_version
+    }
 }
+
 rootProject.name = "crowdsourcing-service"
+
 include("api")
 include("service")
 include("entity")
