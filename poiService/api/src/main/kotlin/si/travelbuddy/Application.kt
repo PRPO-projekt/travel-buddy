d12440734036558b87a5e272f6cee08b8ba99353
Arne <arne.salobir@gmail.com>
2025-01-06 20:46:52 +0100

    Healthy body healthy mind

--- poiService/api/src/main/kotlin/si/travelbuddy/Application.kt
+++ poiService/api/src/main/kotlin/si/travelbuddy/Application.kt
@@ -5,10 +5,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.resources.*
-import si.travelbuddy.plugins.configureDatabases
-import si.travelbuddy.plugins.configureHTTP
-import si.travelbuddy.plugins.configureMonitoring
-import si.travelbuddy.plugins.configureSerialization
+import si.travelbuddy.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
